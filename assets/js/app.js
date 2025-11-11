60672d53efda9d9032dad59a32eac519baef78ac
HungThanh-2305 <hunghothanh152@gmail.com>
2025-11-12 03:58:44 +0700

    Cập nhật file CSS, JS, HTML và thêm thư mục images

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
 document.getElementById("cta-button").addEventListener("click", function() {
   window.location.href = "products.html";
 });
+
+const menuToggle = document.getElementById('mobile-menu');
+const nav = document.getElementById('navbar');
+
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
